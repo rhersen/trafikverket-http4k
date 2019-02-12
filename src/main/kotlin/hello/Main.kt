927037bd1dde619fb6cbc3008982c9f28a8bad5e
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-02-12 21:30:47 +0100

    Minor cleanup

--- src/main/kotlin/hello/Main.kt
+++ src/main/kotlin/hello/Main.kt
@@ -23,8 +23,7 @@ private fun response(request: Request): Response = Response(OK)
                 ?.joinToString(separator = "", transform = ::announcement)
                 .orEmpty())
 
-fun announcement(a: TrainAnnouncement): String =
-        """
+fun announcement(a: TrainAnnouncement): String = """
   <tr>
     <td>${a.AdvertisedTrainIdent ?: "-"}</td>
     <td>${a.advertised()}</td>
@@ -35,8 +34,8 @@ private fun list(location: String?): List<TrainAnnouncement>? {
     val target: Response = JavaHttpClient()(Request(Method.POST, "http://api.trafikinfo.trafikverket.se/v1.2/data.json")
             .with(Header.CONTENT_TYPE of ContentType.APPLICATION_XML)
             .body(xmlBody(location).trimMargin()))
-    try {
-        return Body.auto<Base>()
+    return try {
+        Body.auto<Base>()
                 .toLens()
                 .extract(target)
                 .RESPONSE
@@ -46,24 +45,22 @@ private fun list(location: String?): List<TrainAnnouncement>? {
     } catch (e: Exception) {
         println(e)
         println(target)
-        return emptyList()
+        emptyList()
     }
 }
 
-private fun xmlBody(location: String?): String {
-    return """<REQUEST>
-                    |<LOGIN authenticationkey='$key' />
-                    |<QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation">
-                    |<FILTER>
-                    |<AND>
-                    |<EQ name="ActivityType" value="Avgang" />
-                    |<EQ name="LocationSignature" value="${location ?: 'N'}" />
-                    |<AND>
-                    |<GT name="AdvertisedTimeAtLocation" value="${"$"}dateadd(-00:01:00)" />
-                    |<LT name="AdvertisedTimeAtLocation" value="${"$"}dateadd(00:20:00)" />
-                    |</AND>
-                    |</AND>
-                    |</FILTER>
-                    |</QUERY>
-                    |</REQUEST>"""
-}
+private fun xmlBody(location: String?): String = """<REQUEST>
+                |<LOGIN authenticationkey='$key' />
+                |<QUERY objecttype="TrainAnnouncement" orderby="AdvertisedTimeAtLocation">
+                |<FILTER>
+                |<AND>
+                |<EQ name="ActivityType" value="Avgang" />
+                |<EQ name="LocationSignature" value="${location ?: 'N'}" />
+                |<AND>
+                |<GT name="AdvertisedTimeAtLocation" value="${"$"}dateadd(-00:01:00)" />
+                |<LT name="AdvertisedTimeAtLocation" value="${"$"}dateadd(00:20:00)" />
+                |</AND>
+                |</AND>
+                |</FILTER>
+                |</QUERY>
+                |</REQUEST>"""
