d229a906fd49cd84f694e9268a764249a2c196d3
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-03-06 07:27:54 +0100

    Cache stations until server is restarted

--- src/main/kotlin/hello/Stations.kt
+++ src/main/kotlin/hello/Stations.kt
@@ -6,29 +6,32 @@ import org.http4k.lens.Header
 import org.http4k.routing.ResourceLoader
 
 object Stations {
+    private var cached: Response? = null
+
     fun stationsWrapper(client: HttpHandler): StationsWrapper {
         val readText: String? = ResourceLoader.Classpath().load("cache/stations.json")?.readText()
 
-        return Body.auto<StationsWrapper>()
-                .toLens()
-                .extract(if (readText == null) client(Request(Method.POST, "http://api.trafikinfo.trafikverket.se/v1.2/data.json")
+        val target: Response = if (readText != null) {
+            println("Using downloaded stations")
+            Response(Status.OK).body(readText)
+        } else {
+            if (cached == null) {
+                println("Getting stations from server")
+                cached = client(Request(Method.POST, "http://api.trafikinfo.trafikverket.se/v1.2/data.json")
                         .with(Header.CONTENT_TYPE of ContentType.APPLICATION_XML)
                         .body(stationsQuery().trimMargin()))
-                else Response(Status.OK).body(readText))
+            } else {
+                println("Using cached stations")
+            }
+
+            cached!!
+        }
+
+        return Body.auto<StationsWrapper>().toLens().extract(target)
     }
 
     private fun stationsQuery(): String = """<REQUEST>
      <LOGIN authenticationkey='$key' />
-     <QUERY objecttype='TrainStation'>
-      <FILTER>
-       <OR>
-         <IN name='CountyNo' value='1' />
-         <EQ name='LocationSignature' value='U' />
-         <EQ name='LocationSignature' value='Kn' />
-         <EQ name='LocationSignature' value='Gn' />
-         <EQ name='LocationSignature' value='Bål' />
-       </OR>
-      </FILTER>
-     </QUERY>
+     <QUERY objecttype='TrainStation' />
     </REQUEST>"""
 }
